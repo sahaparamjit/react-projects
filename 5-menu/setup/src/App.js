0c60b4c67a376375700908fc87bcc0cb3f8c5316
Paramjit Saha <sahaparamjit@gmail.com>
2021-05-14 15:05:48 +0000

    Completed the menu exercise

--- 5-menu/setup/src/App.js
+++ 5-menu/setup/src/App.js
@@ -1,10 +1,38 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState, useEffect } from 'react'
+import Menu from './Menu'
+import Categories from './Categories'
+import items from './data'
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items)
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    const uniqueCategories = [
+      'all',
+      ...new Set(items.map((item) => item.category)),
+    ]
+    setCategories(uniqueCategories)
+  }, [])
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items)
+      return
+    }
+    const newItems = items.filter((item) => item.category === category)
+    setMenuItems(newItems)
+  }
+  return (
+    <main className='menu section'>
+      <div className='title'>
+        <h2>our menu</h2>
+        <div className='underline'></div>
+      </div>
+      <Categories categories={categories} filterItems={filterItems} />
+      <Menu items={menuItems} />
+    </main>
+  )
 }
 
-export default App;
+export default App
--- 5-menu/setup/src/Categories.js
+++ 5-menu/setup/src/Categories.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from 'react'
 
-const Categories = () => {
-  return <h2>categories component</h2>;
-};
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className='btn-container'>
+      {categories.map((category, index) => {
+        return (
+          <>
+            <button
+              className='filter-btn'
+              onClick={() => filterItems(category)}
+              key={index}
+            >
+              {category}
+            </button>
+          </>
+        )
+      })}
+    </div>
+  )
+}
 
-export default Categories;
+export default Categories
