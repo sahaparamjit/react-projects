5284c5f7fca158a2887f9eb6902c189b6f3a7c8d
Paramjit Saha <sahaparamjit@gmail.com>
2021-05-15 11:04:44 +0000

    Completed lorem ipsum

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,63 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState, useEffect } from 'react'
+import data from './data'
 function App() {
-  
-  
+  const [textData, setTextData] = useState([])
+  const [val, setVal] = useState(0)
+  const [error, setError] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let amount = parseInt(val)
+    if (amount <= 0 || amount > data.length - 1) {
+      setError(true)
+      amount = 1
+    }
+    const filteredData = data.slice(0, amount)
+    setTextData(filteredData)
+  }
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setError(false)
+    }, 3000)
+    return () => clearInterval(slider)
+  }, [error])
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className='section-center'>
+      {error && (
+        <header style={{ border: '1px solid red' }}>
+          Seems incorrect number of paragraphs or more than 8.
+        </header>
+      )}
+      <form onSubmit={handleSubmit} className='lorem-form'>
+        <label htmlFor='amount'>paragraphs:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
+        />
+        <button type='submit' className='btn'>
+          Generate
+        </button>
+      </form>
+      <article className='lorem-text'>
+        {textData.length !== 0 ? (
+          <span>
+            {textData.map((text, index) => {
+              return (
+                <div className='result' key={index}>
+                  <p>{text}</p>
+                </div>
+              )
+            })}
+          </span>
+        ) : null}
+      </article>
+    </section>
+  )
 }
 
-export default App;
+export default App
